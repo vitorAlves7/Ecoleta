7aadf11e2e0608d76e1d1dccf35812b462880ef7
vitorAlves7 <vitor.alves07750@outlook.com>
2020-06-03 14:59:20 -0300

    This modification were about learning routes concepts and HTTP methods
    Signed-off-by: vitorAlves7 <vitor.alves07750@outlook.com>

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,44 @@
-import express from 'express';
+import express, { request } from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+// Lista de usuários
+const users = [
+    'Diego',
+    'Cleiton',
+    'Robson',
+    'Daniel',
+]
+
 app.get('/users', (request,response) =>{
-    console.log('Listagem de usuários');
-    response.json([
-        'Diego',
-        'Cleiton',
-        'Robson',
-        'Daniel',
-    ]);
+
+    const search = String(request.query.search);
+    console.log(search);
+
+    const filteredUsers = search ?  users.filter(user => user.includes(search)) : users;
+    return response.json(filteredUsers);
+});
+
+app.get('/users/:id',(request,response) =>{
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+});
+
+app.post('/users',(request,response) =>{
+    const data = request.body;
+
+    console.log(data);
+
+    const user = {
+        name: data.name,
+        email: data.email,
+    };
+    return response.json(user);
 });
 
 app.listen(3333);
